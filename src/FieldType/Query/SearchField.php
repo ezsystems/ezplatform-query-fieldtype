bdunogier/ezplatform-query-fieldtype-original

Initial prototype

src/lib/FieldType/Query/SearchField.php
@@ -1,0 +1,75 @@
+<?php
+
+namespace BD\PlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\SPI\Persistence\Content\Field;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\SPI\FieldType\Indexable;
+use eZ\Publish\SPI\Search;
+
+class SearchField implements Indexable
+{
+    /**
+     * Get index data for field for search backend.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDefinition
+     *
+     * @return \eZ\Publish\SPI\Search\Field[]
+     */
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition)
+    {
+        return array(
+            new Search\Field(
+                'value',
+                $field->value->data,
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'fulltext',
+                $field->value->data,
+                new Search\FieldType\FullTextField()
+            ),
+        );
+    }
+
+    /**
+     * Get index field types for search backend.
+     *
+     * @return \eZ\Publish\SPI\Search\FieldType[]
+     */
+    public function getIndexDefinition()
+    {
+        return array(
+            'value' => new Search\FieldType\StringField(),
+        );
+    }
+
+    /**
+     * Get name of the default field to be used for matching.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for matching. Default field is typically used by Field criterion.
+     *
+     * @return string
+     */
+    public function getDefaultMatchField()
+    {
+        return 'value';
+    }
+
+    /**
+     * Get name of the default field to be used for sorting.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for sorting. Default field is typically used by Field sort clause.
+     *
+     * @return string
+     */
+    public function getDefaultSortField()
+    {
+        return $this->getDefaultMatchField();
+    }
+}

src/lib/FieldType/Query/Type.php
@@ -1,0 +1,194 @@
+<?php
+
+namespace BD\PlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\Core\FieldType\FieldType;
+use eZ\Publish\Core\FieldType\ValidationError;
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value as SPIValue;
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Type extends FieldType
+{
+    protected $validatorConfigurationSchema = array();
+
+    protected $settingsSchema = [
+        'QueryType' => ['type' => 'string', 'default' => ''],
+        'Parameters' => ['type' => 'string', 'default' => ''],
+    ];
+
+
+    /**
+     * Validates the validatorConfiguration of a FieldDefinitionCreateStruct or FieldDefinitionUpdateStruct.
+     *
+     * @param mixed $validatorConfiguration
+     *
+     * @return \eZ\Publish\SPI\FieldType\ValidationError[]
+     */
+    public function validateValidatorConfiguration($validatorConfiguration)
+    {
+        $validationErrors = [];
+
+        return $validationErrors;
+    }
+
+    /**
+     * Validates a field based on the validators in the field definition.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     *
+     * @param \eZ\Publish\API\Repository\Values\ContentType\FieldDefinition $fieldDefinition The field definition of the field
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $fieldValue The field value for which an action is performed
+     *
+     * @return \eZ\Publish\SPI\FieldType\ValidationError[]
+     */
+    public function validate(FieldDefinition $fieldDefinition, SPIValue $fieldValue)
+    {
+        $validationErrors = [];
+
+        return $validationErrors;
+    }
+
+    /**
+     * Returns the field type identifier for this field type.
+     *
+     * @return string
+     */
+    public function getFieldTypeIdentifier()
+    {
+        return 'query';
+    }
+
+    /**
+     * Returns the name of the given field value.
+     *
+     * It will be used to generate content name and url alias if current field is designated
+     * to be used in the content name/urlAlias pattern.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     *
+     * @return string
+     */
+    public function getName(SPIValue $value)
+    {
+        return (string)$value->text;
+    }
+
+    public function getEmptyValue()
+    {
+        return new Value();
+    }
+
+    /**
+     * Returns if the given $value is considered empty by the field type.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function isEmptyValue(SPIValue $value)
+    {
+        return $value->text === null || trim($value->text) === '';
+    }
+
+    protected function createValueFromInput($inputValue)
+    {
+        if (is_string($inputValue)) {
+            $inputValue = new Value($inputValue);
+        }
+
+        return $inputValue;
+    }
+
+    /**
+     * Throws an exception if value structure is not of expected format.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If the value does not match the expected structure.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     */
+    protected function checkValueStructure(BaseValue $value)
+    {
+        if (!is_string($value->text)) {
+            throw new InvalidArgumentType(
+                '$value->text',
+                'string',
+                $value->text
+            );
+        }
+    }
+
+    /**
+     * Returns information for FieldValue->$sortKey relevant to the field type.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     *
+     * @return array
+     */
+    protected function getSortInfo(BaseValue $value)
+    {
+        return $this->transformationProcessor->transformByGroup((string)$value, 'lowercase');
+    }
+
+    /**
+     * Converts an $hash to the Value defined by the field type.
+     *
+     * @param mixed $hash
+     *
+     * @return \eZ\Publish\Core\FieldType\TextLine\Value $value
+     */
+    public function fromHash($hash)
+    {
+        if ($hash === null) {
+            return $this->getEmptyValue();
+        }
+
+        return new Value($hash);
+    }
+
+    /**
+     * Converts a $Value to a hash.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     *
+     * @return mixed
+     */
+    public function toHash(SPIValue $value)
+    {
+        if ($this->isEmptyValue($value)) {
+            return null;
+        }
+
+        return $value->text;
+    }
+
+    /**
+     * Returns whether the field type is searchable.
+     *
+     * @return bool
+     */
+    public function isSearchable()
+    {
+        return true;
+    }
+
+    public function validateFieldSettings($fieldSettings)
+    {
+        $errors = [];
+
+        if (isset($fieldSettings['QueryType'])) {
+            /**
+             * $errors[] = new ValidationError("Query type %query_type does not exist", null, ['%query_type%' => $fieldSettings['QueryType']]);
+             */
+        }
+
+        if (isset($fieldSettings['Parameters']) && $fieldSettings['Parameters']) {
+            if (json_decode($fieldSettings['Parameters']) === null) {
+                $errors[] = new ValidationError("Parameters is not a valid json structure");
+            }
+        }
+
+         return $errors;
+    }
+}

src/lib/FieldType/Query/Value.php
@@ -1,0 +1,33 @@
+<?php
+
+namespace BD\PlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Value extends BaseValue
+{
+    /**
+     * Text content.
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Construct a new Value object and initialize it $text.
+     *
+     * @param string $text
+     */
+    public function __construct($text = '')
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @see \eZ\Publish\Core\FieldType\Value
+     */
+    public function __toString()
+    {
+        return (string)$this->text;
+    }
+}

src/lib/Form/Type/FieldType/QueryFieldType.php
@@ -1,0 +1,56 @@
+<?php
+
+namespace BD\PlatformQueryFieldType\Form\Type\FieldType;
+
+use eZ\Publish\API\Repository\FieldTypeService;
+use EzSystems\RepositoryForms\FieldType\DataTransformer\FieldValueTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QueryFieldType extends AbstractType
+{
+    /** @var FieldTypeService */
+    private $fieldTypeService;
+
+    public function __construct(FieldTypeService $fieldTypeService)
+    {
+        $this->fieldTypeService = $fieldTypeService;
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'ezplatform_fieldtype_query';
+    }
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new FieldValueTransformer($this->fieldTypeService->getFieldType('query')));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $attributes = [];
+
+        // $view->vars['QueryType'] = array_merge($view->vars['QueryType'], $attributes);
+        // $view->vars['Test'] = array_merge($view->vars['Test'], $attributes);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([]);
+    }
+}

src/lib/Persistence/Legacy/Content/FieldValue/Converter/QueryConverter.php
@@ -1,0 +1,90 @@
+<?php
+
+namespace BD\PlatformQueryFieldType\Persistence\Legacy\Content\FieldValue\Converter;
+
+use eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\Converter;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue;
+use eZ\Publish\SPI\Persistence\Content\FieldValue;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition;
+
+class QueryConverter implements Converter
+{
+    /**
+     * Factory for current class.
+     *
+     * Note: Class should instead be configured as service if it gains dependencies.
+     *
+     * @deprecated since 6.8, will be removed in 7.x, use default constructor instead.
+     *
+     * @return \eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\Converter\TextLineConverter
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * Converts data from $value to $storageFieldValue.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\FieldValue $value
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue $storageFieldValue
+     */
+    public function toStorageValue(FieldValue $value, StorageFieldValue $storageFieldValue)
+    {
+        $storageFieldValue->dataText = $value->data;
+        $storageFieldValue->sortKeyString = $value->sortKey;
+    }
+
+    /**
+     * Converts data from $value to $fieldValue.
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue $value
+     * @param \eZ\Publish\SPI\Persistence\Content\FieldValue $fieldValue
+     */
+    public function toFieldValue(StorageFieldValue $value, FieldValue $fieldValue)
+    {
+        $fieldValue->data = $value->dataText;
+        $fieldValue->sortKey = $value->sortKeyString;
+    }
+
+    /**
+     * Converts field definition data in $fieldDef into $storageFieldDef.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDef
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition $storageDef
+     */
+    public function toStorageFieldDefinition(FieldDefinition $fieldDef, StorageFieldDefinition $storageDef)
+    {
+        $storageDef->dataText1 = $fieldDef->fieldTypeConstraints->fieldSettings['QueryType'];
+        $storageDef->dataText5 = $fieldDef->fieldTypeConstraints->fieldSettings['Parameters'];
+    }
+
+    /**
+     * Converts field definition data in $storageDef into $fieldDef.
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition $storageDef
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDef
+     */
+    public function toFieldDefinition(StorageFieldDefinition $storageDef, FieldDefinition $fieldDef)
+    {
+        $fieldDef->fieldTypeConstraints->fieldSettings = [
+            'QueryType' => $storageDef->dataText1 ?: null,
+            'Parameters' => $storageDef->dataText5 ?: ''
+        ];
+    }
+
+    /**
+     * Returns the name of the index column in the attribute table.
+     *
+     * Returns the name of the index column the datatype uses, which is either
+     * "sort_key_int" or "sort_key_string". This column is then used for
+     * filtering and sorting for this type.
+     *
+     * @return string
+     */
+    public function getIndexColumn()
+    {
+        return 'sort_key_string';
+    }
+}

ezsystems/ezplatform-query-fieldtype

src/FieldType/Query/SearchField.php
@@ -1,0 +1,75 @@
+<?php
+
+namespace EzSystems\EzPlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\SPI\Persistence\Content\Field;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\SPI\FieldType\Indexable;
+use eZ\Publish\SPI\Search;
+
+class SearchField implements Indexable
+{
+    /**
+     * Get index data for field for search backend.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDefinition
+     *
+     * @return \eZ\Publish\SPI\Search\Field[]
+     */
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition)
+    {
+        return array(
+            new Search\Field(
+                'value',
+                $field->value->data,
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'fulltext',
+                $field->value->data,
+                new Search\FieldType\FullTextField()
+            ),
+        );
+    }
+
+    /**
+     * Get index field types for search backend.
+     *
+     * @return \eZ\Publish\SPI\Search\FieldType[]
+     */
+    public function getIndexDefinition()
+    {
+        return array(
+            'value' => new Search\FieldType\StringField(),
+        );
+    }
+
+    /**
+     * Get name of the default field to be used for matching.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for matching. Default field is typically used by Field criterion.
+     *
+     * @return string
+     */
+    public function getDefaultMatchField()
+    {
+        return 'value';
+    }
+
+    /**
+     * Get name of the default field to be used for sorting.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for sorting. Default field is typically used by Field sort clause.
+     *
+     * @return string
+     */
+    public function getDefaultSortField()
+    {
+        return $this->getDefaultMatchField();
+    }
+}

src/FieldType/Query/Type.php
@@ -1,0 +1,195 @@
+<?php
+
+namespace EzSystems\EzPlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\Core\FieldType\FieldType;
+use eZ\Publish\Core\FieldType\ValidationError;
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value as SPIValue;
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Type extends FieldType
+{
+    protected $validatorConfigurationSchema = array();
+
+    protected $settingsSchema = [
+        'QueryType' => ['type' => 'string', 'default' => ''],
+        'Parameters' => ['type' => 'string', 'default' => ''],
+        'ReturnedType' => ['type' => 'string', 'default' => ''],
+    ];
+
+
+    /**
+     * Validates the validatorConfiguration of a FieldDefinitionCreateStruct or FieldDefinitionUpdateStruct.
+     *
+     * @param mixed $validatorConfiguration
+     *
+     * @return \eZ\Publish\SPI\FieldType\ValidationError[]
+     */
+    public function validateValidatorConfiguration($validatorConfiguration)
+    {
+        $validationErrors = [];
+
+        return $validationErrors;
+    }
+
+    /**
+     * Validates a field based on the validators in the field definition.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     *
+     * @param \eZ\Publish\API\Repository\Values\ContentType\FieldDefinition $fieldDefinition The field definition of the field
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $fieldValue The field value for which an action is performed
+     *
+     * @return \eZ\Publish\SPI\FieldType\ValidationError[]
+     */
+    public function validate(FieldDefinition $fieldDefinition, SPIValue $fieldValue)
+    {
+        $validationErrors = [];
+
+        return $validationErrors;
+    }
+
+    /**
+     * Returns the field type identifier for this field type.
+     *
+     * @return string
+     */
+    public function getFieldTypeIdentifier()
+    {
+        return 'query';
+    }
+
+    /**
+     * Returns the name of the given field value.
+     *
+     * It will be used to generate content name and url alias if current field is designated
+     * to be used in the content name/urlAlias pattern.
+     *
+     * @param \EzSystems\EzPlatformQueryFieldType\FieldType\Query\Value $value
+     *
+     * @return string
+     */
+    public function getName(SPIValue $value)
+    {
+        return (string)$value->text;
+    }
+
+    public function getEmptyValue()
+    {
+        return new Value();
+    }
+
+    /**
+     * Returns if the given $value is considered empty by the field type.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function isEmptyValue(SPIValue $value)
+    {
+        return false;
+    }
+
+    protected function createValueFromInput($inputValue)
+    {
+        if (is_string($inputValue)) {
+            $inputValue = new Value($inputValue);
+        }
+
+        return $inputValue;
+    }
+
+    /**
+     * Throws an exception if value structure is not of expected format.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If the value does not match the expected structure.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     */
+    protected function checkValueStructure(BaseValue $value)
+    {
+        if (!is_string($value->text)) {
+            throw new InvalidArgumentType(
+                '$value->text',
+                'string',
+                $value->text
+            );
+        }
+    }
+
+    /**
+     * Returns information for FieldValue->$sortKey relevant to the field type.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     *
+     * @return array
+     */
+    protected function getSortInfo(BaseValue $value)
+    {
+        return $this->transformationProcessor->transformByGroup((string)$value, 'lowercase');
+    }
+
+    /**
+     * Converts an $hash to the Value defined by the field type.
+     *
+     * @param mixed $hash
+     *
+     * @return \eZ\Publish\Core\FieldType\TextLine\Value $value
+     */
+    public function fromHash($hash)
+    {
+        if ($hash === null) {
+            return $this->getEmptyValue();
+        }
+
+        return new Value($hash);
+    }
+
+    /**
+     * Converts a $Value to a hash.
+     *
+     * @param \eZ\Publish\Core\FieldType\TextLine\Value $value
+     *
+     * @return mixed
+     */
+    public function toHash(SPIValue $value)
+    {
+        if ($this->isEmptyValue($value)) {
+            return null;
+        }
+
+        return $value->text;
+    }
+
+    /**
+     * Returns whether the field type is searchable.
+     *
+     * @return bool
+     */
+    public function isSearchable()
+    {
+        return true;
+    }
+
+    public function validateFieldSettings($fieldSettings)
+    {
+        $errors = [];
+
+        if (isset($fieldSettings['QueryType'])) {
+            /**
+             * $errors[] = new ValidationError("Query type %query_type does not exist", null, ['%query_type%' => $fieldSettings['QueryType']]);
+             */
+        }
+
+        if (isset($fieldSettings['Parameters']) && $fieldSettings['Parameters']) {
+            if (json_decode($fieldSettings['Parameters']) === null) {
+                $errors[] = new ValidationError("Parameters is not a valid json structure");
+            }
+        }
+
+         return $errors;
+    }
+}

src/FieldType/Query/Value.php
@@ -1,0 +1,33 @@
+<?php
+
+namespace EzSystems\EzPlatformQueryFieldType\FieldType\Query;
+
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Value extends BaseValue
+{
+    /**
+     * Text content.
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Construct a new Value object and initialize it $text.
+     *
+     * @param string $text
+     */
+    public function __construct($text = '')
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @see \eZ\Publish\Core\FieldType\Value
+     */
+    public function __toString()
+    {
+        return (string)$this->text;
+    }
+}

src/Form/Type/FieldType/QueryFieldType.php
@@ -1,0 +1,56 @@
+<?php
+
+namespace EzSystems\EzPlatformQueryFieldType\Form\Type\FieldType;
+
+use eZ\Publish\API\Repository\FieldTypeService;
+use EzSystems\RepositoryForms\FieldType\DataTransformer\FieldValueTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QueryFieldType extends AbstractType
+{
+    /** @var FieldTypeService */
+    private $fieldTypeService;
+
+    public function __construct(FieldTypeService $fieldTypeService)
+    {
+        $this->fieldTypeService = $fieldTypeService;
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'ezplatform_fieldtype_query';
+    }
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new FieldValueTransformer($this->fieldTypeService->getFieldType('query')));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $attributes = [];
+
+        // $view->vars['QueryType'] = array_merge($view->vars['QueryType'], $attributes);
+        // $view->vars['Test'] = array_merge($view->vars['Test'], $attributes);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([]);
+    }
+}

src/Persistence/Legacy/Content/FieldValue/Converter/QueryConverter.php
@@ -1,0 +1,92 @@
+<?php
+
+namespace EzSystems\EzPlatformQueryFieldType\Persistence\Legacy\Content\FieldValue\Converter;
+
+use eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\Converter;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue;
+use eZ\Publish\SPI\Persistence\Content\FieldValue;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition;
+
+class QueryConverter implements Converter
+{
+    /**
+     * Factory for current class.
+     *
+     * Note: Class should instead be configured as service if it gains dependencies.
+     *
+     * @deprecated since 6.8, will be removed in 7.x, use default constructor instead.
+     *
+     * @return \eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\Converter\TextLineConverter
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * Converts data from $value to $storageFieldValue.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\FieldValue $value
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue $storageFieldValue
+     */
+    public function toStorageValue(FieldValue $value, StorageFieldValue $storageFieldValue)
+    {
+        $storageFieldValue->dataText = $value->data;
+        $storageFieldValue->sortKeyString = $value->sortKey;
+    }
+
+    /**
+     * Converts data from $value to $fieldValue.
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue $value
+     * @param \eZ\Publish\SPI\Persistence\Content\FieldValue $fieldValue
+     */
+    public function toFieldValue(StorageFieldValue $value, FieldValue $fieldValue)
+    {
+        $fieldValue->data = $value->dataText;
+        $fieldValue->sortKey = $value->sortKeyString;
+    }
+
+    /**
+     * Converts field definition data in $fieldDef into $storageFieldDef.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDef
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition $storageDef
+     */
+    public function toStorageFieldDefinition(FieldDefinition $fieldDef, StorageFieldDefinition $storageDef)
+    {
+        $storageDef->dataText1 = $fieldDef->fieldTypeConstraints->fieldSettings['QueryType'];
+        $storageDef->dataText2 = $fieldDef->fieldTypeConstraints->fieldSettings['ReturnedType'];
+        $storageDef->dataText5 = $fieldDef->fieldTypeConstraints->fieldSettings['Parameters'];
+    }
+
+    /**
+     * Converts field definition data in $storageDef into $fieldDef.
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition $storageDef
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDef
+     */
+    public function toFieldDefinition(StorageFieldDefinition $storageDef, FieldDefinition $fieldDef)
+    {
+        $fieldDef->fieldTypeConstraints->fieldSettings = [
+            'QueryType' => $storageDef->dataText1 ?: null,
+            'ReturnedType' => $storageDef->dataText2 ?: null,
+            'Parameters' => $storageDef->dataText5 ?: ''
+        ];
+    }
+
+    /**
+     * Returns the name of the index column in the attribute table.
+     *
+     * Returns the name of the index column the datatype uses, which is either
+     * "sort_key_int" or "sort_key_string". This column is then used for
+     * filtering and sorting for this type.
+     *
+     * @return string
+     */
+    public function getIndexColumn()
+    {
+        return 'sort_key_string';
+    }
+}